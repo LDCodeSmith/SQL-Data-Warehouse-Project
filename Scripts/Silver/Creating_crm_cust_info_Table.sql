7cce0f1d8f2e31364977284fed1c94c8a8fbcfe7
xblackbagx-pixel <xblackbagx@gmail.com>
2026-05-10 10:38:42 -0400

    Create Silver crm_cust_info table with transformations

---
+++ Scripts/Silver/Creating_crm_cust_info_Table.sql
@@ -0,0 +1,33 @@
+--Creating Silver crm_cust_info Table through Transformations
+
+USE DataWarehouse
+INSERT INTO Silver.crm_cust_info (
+	cst_id,
+	cst_key,
+	cst_firstname,
+	cst_lastname,
+	cst_marital_status,
+	cst_gndr,
+	cst_create_date
+)
+SELECT 
+	cst_id, 
+	cst_key,
+	TRIM(cst_firstname) AS cst_firstname,
+	TRIM(cst_lastname) AS cst_lastname,
+	CASE WHEN UPPER(cst_marital_status) = 'S' THEN 'Single'
+		 WHEN UPPER(cst_marital_status) = 'M' THEN 'Married'
+		 ELSE 'n/a'
+	END cst_marital_status,
+	CASE WHEN UPPER(cst_gndr) = 'F' THEN 'Female'
+		 WHEN UPPER(cst_gndr) = 'M' THEN 'Male'
+		 ELSE 'n/a'
+	END cst_gndr,
+	cst_create_date
+FROM ( 
+SELECT 
+*,
+ROW_NUMBER() OVER (PARTITION BY cst_id ORDER BY cst_create_date DESC) AS Flag_last
+FROM bronze.crm_cust_info
+WHERE cst_id IS NOT NULL 
+)t WHERE flag_last = 1
